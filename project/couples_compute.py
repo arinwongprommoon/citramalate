21487f1245d87dc7137f32a9c90da66cb45db180
arinwongprommoon <bht9216@gmail.com>
2018-07-10 15:20:00 +0100

    made additional heatmaps

--- project/couples_compute.py
+++ project/couples_compute.py
@@ -11,13 +11,13 @@ import numpy as np
 from couples_noloop import Coupl
 
 #Run simulations, writes data both into TXT and NPZ
-run = Coupl(xstart=0.1, xend=1.0, ystart=0.1, yend=1.0, points=10) # define vmax ranges here
+run = Coupl(xstart=0.1, xend=10.0, ystart=0.1, yend=10.0, points=10) # define vmax ranges here
 run.setVmax('CITRA_SYN', 4.0)
 run.time0 = 0
 run.timef = 2*3600 # final simulation time in seconds
 run.npoints = 100 # Number of points to be computed in the simulation
-run.listofreactions = ['CITRA_SYN', 'GLT', 'LPD', 'GDH','ATP_syn'] # define list of reactions
-#run.listofreactions = ['CITRA_SYN', 'GLT', 'LPD', 'ATP_MAINTENANCE', 'GDH', 'ATP_syn', 'ACEA', 'ZWF']
+#run.listofreactions = ['CITRA_SYN', 'GLT', 'LPD', 'ATP_MAINTENANCE', 'GDH', 'ATP_syn'] # define list of reactions
+run.listofreactions = ['CITRA_SYN', 'GLT', 'LPD', 'ATP_MAINTENANCE', 'GDH', 'ATP_syn', 'ACEA', 'ZWF']
 
 run.writeFileHeader()
 run.computeCouples()
