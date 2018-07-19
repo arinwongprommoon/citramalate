1b3a31d08c3316175530a31a708ec337d084c099
arinwongprommoon <bht9216@gmail.com>
2018-07-19 11:50:39 +0100

    use new parameter values for de
    - Used Pedersen's recommended values for D = 10,
      fitness evaluations = 2000 on 7 glycolytic enzymes:
    
      NP = 28, CR = 0.9426, F = 0.6607

--- kinetic/de.py
+++ kinetic/de.py
@@ -44,7 +44,7 @@ def productivity(r, x):
 generations = []
 
 # DE algorithm adapted from Pablo R Mier
-def de(fobj, bounds, mut=0.6301, crossp=0.7122, popsize=17, its=70):
+def de(fobj, bounds, mut=0.6607, crossp=0.9426, popsize=28, its=50):
     dimensions = len(bounds)
     # Initialisation
     pop = np.random.rand(popsize, dimensions)
