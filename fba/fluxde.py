a3ae91288ccefdcb5710c421c7bcd0920e7e6707
arinwongprommoon <bht9216@gmail.com>
2018-07-25 10:02:08 +0100

    fix: GLC_feed in fluxde does not return results
    - Found out that the reason is that fitness never changes from 0.23,
      so when the DE algorithm is running (in de()), it never goes into
      selection and as a consequence, never yields best and
      fitness[best_idx]
    - And because including GLC_feed in the flux constraints in FBA
      causes unfeasible solutions, it's probably best to just leave it
      out. (Will investigate why it does this later)

--- fba/fluxde.py
+++ fba/fluxde.py
@@ -8,7 +8,9 @@ import libsbml
 import time
 import sys
 
-allreactions = ['PGI', 'PFK', 'FBA', 'TPI', 'GDH', 'PGK', 'GPM', 'ENO', 'PYK', 'ZWF', 'PGL', 'GND', 'RPE', 'RPI', 'X5P_GAP_TKT', 'F6P_E4P_TKT', 'S7P_R5P_TKT', 'F6P_GAP_TAL', 'S7P_E4P_TAL', 'FBP', 'PPC', 'PCK', 'PPS', 'MAD', 'PDH', 'GLT', 'ACN_1', 'ACN_2', 'ICD', 'LPD', 'SK', 'SDH', 'FUMA', 'MQO', 'MDH', 'ACEA', 'ACEB', 'ACEK_1', 'ACEK_2', 'EDD', 'EDA', 'NADH_req', 'PNT_req', 'ADK', 'ATP_syn', 'CYA', 'DOS', 'ACK', 'ACS', 'PTA', 'PTS_0', 'PTS_1', 'PTS_2', 'PTS_3', 'PTS_4', 'GLC_feed', 'CYTBO', 'SQR', 'NDHII', 'GROWTH', 'ATP_MAINTENANCE', 'XCH_GLC', 'PIT', 'XCH_P', 'XCH_ACE1', '_ACE_OUT', 'XCH_ACE2', 'GL6P_HYDROLYSIS']
+#allreactions = ['PGI', 'PFK', 'FBA', 'TPI', 'GDH', 'PGK', 'GPM', 'ENO', 'PYK', 'ZWF', 'PGL', 'GND', 'RPE', 'RPI', 'X5P_GAP_TKT', 'F6P_E4P_TKT', 'S7P_R5P_TKT', 'F6P_GAP_TAL', 'S7P_E4P_TAL', 'FBP', 'PPC', 'PCK', 'PPS', 'MAD', 'PDH', 'GLT', 'ACN_1', 'ACN_2', 'ICD', 'LPD', 'SK', 'SDH', 'FUMA', 'MQO', 'MDH', 'ACEA', 'ACEB', 'ACEK_1', 'ACEK_2', 'EDD', 'EDA', 'NADH_req', 'PNT_req', 'ADK', 'ATP_syn', 'CYA', 'DOS', 'ACK', 'ACS', 'PTA', 'PTS_0', 'PTS_1', 'PTS_2', 'PTS_3', 'PTS_4', 'GLC_feed', 'CYTBO', 'SQR', 'NDHII', 'GROWTH', 'ATP_MAINTENANCE', 'XCH_GLC', 'PIT', 'XCH_P', 'XCH_ACE1', '_ACE_OUT', 'XCH_ACE2', 'GL6P_HYDROLYSIS']
+
+allreactions = ['PTS_4', 'GLC_feed', 'CYTBO', 'SQR']
 
 # Reads wild-type model
 reader = libsbml.SBMLReader()
@@ -116,7 +118,7 @@ with open('j.txt', 'r') as fobj:
     mystr = fobj.readline()
     jdx = int(mystr.strip('\n'))
     
-if jdx >= 68:
+if jdx >= len(allreactions):
     print('DONE')
     sys.exit(1)
     
@@ -134,8 +136,8 @@ else:
         print(bounds)
 
         def fobj(x):
-            #return flux(jdx, combo, x) # MINIMUM
-            return -flux(jdx, combo, x) # MAXIMUM
+            return flux(54+jdx, combo, x) # MINIMUM
+            #return -flux(54+jdx, combo, x) # MAXIMUM
 
         # computation
         result = list(de(fobj, bounds))
