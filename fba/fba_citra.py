3ed5d9cfaa9ec63f1492447145ade45ca66916aa
arinwongprommoon <bht9216@gmail.com>
2018-10-04 15:50:01 +0100

    Growth: used growth as obj func and recorded results

--- fba/fba_citra.py
+++ fba/fba_citra.py
@@ -8,7 +8,7 @@ import cobra.test
 from cobra import Reaction, Metabolite
 
 # DEFINE FILE TO BE LOADED FOR FBA BOUNDS HERE
-loc = 'boundaries_citra/AnargyrosBoundaries.csv'
+loc = 'boundaries_citra/41dBoundaries.csv'
 
 modelfile = "MODEL1108160000" # DEFINE MODEL FILE HERE
 #objective = 'CitraSink' # DEFINE OBJECTIVE REACTION HERE
