754e79054d95213dec012f3bb57624e200d60d42
arinwongprommoon <bht9216@gmail.com>
2019-08-08 23:05:45 +0700

    commit

--- fba/fba_citra.py
+++ fba/fba_citra.py
@@ -8,7 +8,7 @@ import cobra.test
 from cobra import Reaction, Metabolite
 
 # DEFINE FILE TO BE LOADED FOR FBA BOUNDS HERE
-loc = 'boundaries_citra/8dBoundaries_20190808.csv'
+loc = 'boundaries_citra/8dBoundaries_10g.csv'
 
 modelfile = "MODEL1108160000" # DEFINE MODEL FILE HERE
 objective = 'CitraSink' # DEFINE OBJECTIVE REACTION HERE
