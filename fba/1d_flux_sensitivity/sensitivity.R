efa3c3d2790636c2c30dabf983097f97af0a0ed3
arinwongprommoon <bht9216@gmail.com>
2019-08-16 16:56:34 +0700

    calculated a distance matrix for the flux bounds from 0.1~1.0 Vmax, 0.1~2.0 Vmax to 0.1~10.0 Vmax
    - datapoints.ods is a sheet for me to determine the number of data
      points for each run to roughly conserve the range of Vmax per
      data point
    - sensitivity.R is the code to generate the distance matrix

--- fba/1d_flux_sensitivity/sensitivity.R
+++ fba/1d_flux_sensitivity/sensitivity.R
@@ -4,8 +4,17 @@ for (ii in 1:10){
     allfluxes[[ii]] <- read.csv(path, header=FALSE)
 }
 
+distances <- matrix(1:100, nrow=10, ncol=10)
+for (kk in 1:10){
+    distances[kk, kk] <- 0
+}
 combinations <- combn(1:10, 2, simplify=FALSE)
+options(digits = 3)
 for (jj in combinations){
     print(jj)
-    print(sum(abs(allfluxes[[jj[1]]] - allfluxes[[jj[2]]])))
+    dd = (sum(abs(allfluxes[[jj[1]]] - allfluxes[[jj[2]]])))
+    print(dd)
+    distances[jj[1], jj[2]] <- dd
+    distances[jj[2], jj[1]] <- dd
 }
+print(distances)
