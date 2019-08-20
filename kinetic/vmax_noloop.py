33b7eb41cc2671057648e539c9154f7a9b83fe68
arinwongprommoon <bht9216@gmail.com>
2019-08-20 18:53:02 +0700

    generated one-reaction list with flux through LPD as objective

--- kinetic/vmax_noloop.py
+++ kinetic/vmax_noloop.py
@@ -31,7 +31,7 @@ end = 5.0 # END VALUE IN MULTIPLES OF WT VMAX
 points = 20 # NUMBER OF DATA POINTS
 
 # Reaction concerned
-fluxreac="EDD"
+fluxreac="LPD"
 
 # read the index of the reaction from file
 with open('vmaxi.txt', 'r') as fobj:
